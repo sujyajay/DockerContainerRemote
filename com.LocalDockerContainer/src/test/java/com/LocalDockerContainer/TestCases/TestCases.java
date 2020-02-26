7463b36594b9d43efa9acb15957f9540d482e478
Batman <Batman@DESKTOP-9M4JE7O>
2020-02-26 13:21:19 -0600

    First commit on V1.0

---
+++ com.LocalDockerContainer/src/test/java/com/LocalDockerContainer/TestCases/TestCases.java
@@ -0,0 +1,14 @@
+package com.LocalDockerContainer.TestCases;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	
+	@Test
+	public static void mainTest() {
+		
+		System.out.println("Test Only");
+		
+	}
+	
+}
